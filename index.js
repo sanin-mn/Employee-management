bc88ca4282f520d470881acc6112ec197676d61b
sanin-mn <msanin7003@gmail.com>
2023-09-11 09:29:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// steps to generate json server
+// ..............................
+
+// 1. import json server
+const jsonServer = require('json-server')
+
+// 2. create our own server to run json file
+const server = jsonServer.create()
+
+// 3. generate a middleware to use in json server
+const middleware = jsonServer.defaults()
+
+// 4. set up path for db.json
+const router = jsonServer.router("db.json")
+
+// 5. set up port for server
+const port = 3000 || process.env.PORT
+
+// 6. use middleware, router, to server
+server.use(middleware)
+server.use(router)
+
+// 7. server listen or run to request
+server.listen(port,()=>{
+    console.log("Employee server started at port no : "+port);
+})
